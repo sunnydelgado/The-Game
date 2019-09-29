45409d452a6eefc292b3a56f373fe090fd94bddb
Sunny Tavares Delgado <54977970+sunnydelgado@users.noreply.github.com>
2019-09-29 21:30:14 +0200

    Add files via upload

---
+++ Tonnenjager.js
@@ -0,0 +1,170 @@
+//Tonnenjager
+//Sunny Tavares Delgado
+//AO Davinci College
+
+alert("Welkom bij de Tonnenjager")
+var name= prompt( "Wat is uw naam")
+alert("Welkom " + name + ", Welkom bij de Tonnenjager \nWaar je € 100.000 kan winnen ");
+alert("Je zal nu een aantal vragen krijgen en wie weet ben jij onze winnaar.")
+alert("Laten we beginnen!");
+//Vraag 1
+hoofdstad = prompt("Wat is de hoofdstad van Amerika?\n (Typ 1): Washington D.C.\n (Typ 2): New York City ");
+
+if (hoofdstad==1){
+  alert("Dat is de correcte antwoord!");
+  alert("Je hebt 1 van de 10 vragen correcte");
+  alert("We gaan nu naar vraag 2");
+
+  //Vraag 2
+  oprichter = prompt("Door wie is Apple opgericht?\n (Typ 1): Bill gates \n (typ 2): Steve Woznaik  \n (typ 3): Steve Jobs");
+
+  if(oprichter== 3){
+    alert("Dat is de correcte antwoord!");
+    alert("Je hebt 2 van de 10 vragen correcte");
+    alert("We gaan nu naar vraag 3");
+
+    // Vraag 3
+    artiest = prompt("Wie stond het langst in de billboard hot 100?\n (Typ 1): Mariah Carey\n (Typ 2): lil Nas X \n (Typ 3): Drake \n (Typ 4): Michael Jackson");
+    if(artiest== 2){
+      alert("Dat is de correcte antwoord!");
+      alert("Je hebt 3 van de 10 vragen correcte");
+      alert("We gaan nu naar vraag 4");
+
+      //vraag4
+
+      voetbalclub = prompt("1. Welke Italiaanse voetbalclub wordt vergeleken met een oude dame?\n (Typ 1): Atalanta\n (Typ 2): AS Roma\n (Typ 3): Juventus\n (typ 4): Fioretina");
+
+      if(voetbalclub== 3){
+        alert("Dat is de correcte antwoord!");
+        alert("Je hebt 4 van de 10 vragen correcte");
+        alert("We gaan nu naar vraag 5");
+
+        // Vraag 5
+        zangeres = prompt("Welke R&B-zangeres en actrice overleed in 2001 bij een vliegtuigongeluk?\n (Typ 1): Beyonce\n (Typ 2): Aaliyah\n (Typ 3): Jennifer Lopez");
+
+        if(zangeres== 2){
+          alert("Dat is de correcte antwoord!");
+          alert("Je hebt 5 van de 10 vragen correcte")
+          alert("Je ontavngt een sleutel waarmee je aan het einde een van de 3 kluizen kan openen")
+          alert("in een van de kluizen zit een geld prijs van € 20.000")
+
+
+            land = prompt(" In welk land kwam Expeditie Robinson voor het eerst op tv?\n (Typ 1): Nederland\n (Typ 2): Spanje\n (Typ 3): België\n (Typ 4): Zweden\n\n 1 sleutel");
+
+            if(land== 4){
+              alert("Dat is de correcte antwoord!");
+              alert("Je hebt 6 van de 10 vragen correcte");
+              alert("We gaan nu naar vraag 7");
+
+              //vraag 7
+
+                jackass = prompt("Wat is de naam van de derde bioscoopfilm van de serie Jackass?\n (Typ 1): Jackass 3D\n (Typ 2): Jackass 3.5\n (Typ 3): Jackass 4\n\n 1 sleutel");
+
+                if(jackass== 1 ){
+                  alert("Dat is de correcte antwoord!");
+                  alert("Je hebt 7 van de 10 vragen correcte");
+                  alert("We gaan nu naar vraag 8");
+
+                  //Vraag 8
+
+                  clown = prompt("Hoe heet de clown uit de horrorserie It?\n (Typ 1): Pipo de clown\n (Typ 2): Pennywise the Dancing Clown\n (Typ 3): Bassie de clowm\n\n 1 sleutel ");
+
+                  if(clown== 2 ){
+                    alert("Dat is de correcte antwoord!");
+                    alert("Je hebt 8 van de 10 vragen correcte");
+                    alert("We gaan nu naar vraag 9");
+
+                    //Vraag 9
+
+                    afkorting = prompt("Waarvoor staat de P in de afkorting APK?\n (Typ 1): Periode\n (Typ 2): Pijl\n (Typ 3): Pin\n (Typ 4): Periodieke\n\n 1 sleutel ");
+
+                    if(afkorting== 4 ){
+                      alert("Dat is de correcte antwoord!");
+                      alert("Je hebt 9 van de 10 vragen correcte");
+                      alert("We gaan nu naar de laatste vraag");
+                      alert("we zijn nu bij de laatse vraag beland");
+                      alert("Wie weet bent u gelijk een € 100.000 rijker");
+                      alert("Laten we beginnen");
+
+                      //Vraag 10
+
+                      snaren = prompt("Hoeveel snaren heeft een gitaar gewoonlijk?\n (Typ 1): 8\n (Typ 2): 7\n (Typ 3): 8\n (Typ 4): 6\n\n 1 sleutel");
+
+                      if(snaren== 4 ){
+                        alert("Dat is de correcte antwoord!");
+                        alert("GEFELICITEERD MET JE PRIJS VAN € 100.000!!!!");
+
+                        //Bonus
+
+                        kluis = prompt("Kies uit welke kluis je wilt openen\n (Typ 1): 1\n (Typ 2): 2\n (Typ 3): 3\n\n 1 sleutel");
+
+                        if(kluis == 2){
+                          alert("U heeft nog 20.000 extra gewonnen");
+                          alert("U bent bent nu 120.000 rijker, GEFELICITEERD!!!!");
+                        }
+                        else if(kluis== 1) {
+                          alert("Jammer dat is de verkeerde kluis");
+                        }
+                        else if(kluis== 3) {
+                          alert("Jammer dat is de verkeerde kluis");
+}
+}
+    else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+    }
+    }
+    else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+    }
+    }
+    else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+  }
+  else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+  }
+  else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+  }
+  else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+  }
+  else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+}
+  else {
+    alert("Jammer maar je antwoord is onjuist");
+    alert("GAME OVER!");
+    window.location=window.location
+  }
+  	}
+    else {
+      alert("Jammer maar je antwoord is onjuist");
+      alert("GAME OVER!");
+      window.location=window.location
+    }
+  }
+    else {
+      alert("Jammer maar je antwoord is onjuist");
+      alert("GAME OVER!");
+      window.location=window.location
+    }
